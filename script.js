e4f699737b173787b5c9b5031baff8134f31d16a
snatvb <snatlip48@gmail.com>
2014-06-15 01:18:29 +0400

    Пример активации

--- script.js
+++ script.js
@@ -3,14 +3,4 @@
     _.start();
     _.checkbox.start();
     _.radiobutton.start();
-    return $("#js-slider").slider({
-      value: 65,
-      min: 0,
-      max: 100,
-      step: 1,
-      create: function(event, ui) {
-        return $("#js-slider").slider("value");
-      }
-    });
-  });
 }).call(this);
